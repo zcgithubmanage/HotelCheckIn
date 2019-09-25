940a158cae9cce95dcef3ea246d2655a85c80594
zcgithubmanage <3167955009@qq.com>
2019-09-25 09:54:23 +0800

    测试

--- app/src/main/java/com/jgkj/hotelsystem/MainActivity.kt
+++ app/src/main/java/com/jgkj/hotelsystem/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.jgkj.hotelsystem
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jgkj.hotelsystem.ui.AddressChooseActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        main_main.setOnClickListener {
+            val intentName = Intent(this,AddressChooseActivity::class.java)
+            startActivity(intentName)
+        }
     }
 }
---
+++ app/src/main/java/com/jgkj/hotelsystem/ui/AddressChooseActivity.java
@@ -0,0 +1,21 @@
+package com.jgkj.hotelsystem.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.jgkj.hotelsystem.R;
+
+public class AddressChooseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        helloKottly();
+    }
+
+    private static void helloKottly(){
+
+    }
+}
